6a3de2e3991535e65d2d39f6e90fdf7e8f5334e4
91799 <91799@192.168.1.4>
2024-08-08 19:20:01 +0530

    test commit

---
+++ Rejistration-login-spring-boot-security-thymeleaf/src/main/java/in/Sairamya/RejistrationLoginSpringBootSecurityThymeleafApplication.java
@@ -0,0 +1,13 @@
+package in.Sairamya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RejistrationLoginSpringBootSecurityThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RejistrationLoginSpringBootSecurityThymeleafApplication.class, args);
+	}
+
+}
---
+++ Rejistration-login-spring-boot-security-thymeleaf/src/test/java/in/Sairamya/RejistrationLoginSpringBootSecurityThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package in.Sairamya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RejistrationLoginSpringBootSecurityThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
